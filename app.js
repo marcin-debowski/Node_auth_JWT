b9e5336d00cec61b4689488ad6ad3aa68f592f3f
marcin <marcing.debowski@gmail.com>
2025-07-16 15:39:46 +0200

    signin, login add

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes')
+const cookieParser = require('cookie-parser');
+
 const app = express();
 
 // middleware
 app.use(express.static('public'));
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -23,3 +26,18 @@ mongoose.connect(dbURI)
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+//cookies
+// app.get('/set-cookies', (req,res)=>{
+//   //res.setHeader('Set-Cookies','newUser=true')
+//   res.cookie('newUser',false);
+//   res.cookie('isEmploy',true,{httpOnly:true});
+
+//   res.send('you git cookies')
+// })
+// app.use('/read-cookies',(req,res)=>{
+
+//   const cookies = req.cookies;
+//   res.json(cookies);
+
+// })
